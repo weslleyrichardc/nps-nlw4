d948f9245279a0589d37601609151c79206bb9e4
Weslley Richard <weslleyrichardc@gmail.com>
2021-02-28 15:10:23 -0300

    Aula 4 - Envio de E-mail

--- src/repositories/UsersRepository.ts
+++ src/repositories/UsersRepository.ts
@@ -2,8 +2,6 @@ import { EntityRepository, Repository } from "typeorm";
 import { User } from '../models/User';
 
 @EntityRepository(User)
-class UsersRepository extends Repository<User> {
-    
-}
+class UsersRepository extends Repository<User> {}
 
 export { UsersRepository };
